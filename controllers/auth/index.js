390a31f820d45e3ce836b00c3dc89dc111ab0e2e
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-06-29 01:10:36 +0530

    Add user model , auth routes , auth controller

---
+++ controllers/auth/index.js
@@ -0,0 +1,35 @@
+const db = require('../../config/mongoose');
+
+module.exports.authUser = async (req,res) => {
+    try{
+        const {username , password} = req.body;
+        let user = await db.collection('User').findOne({ username });
+        if( user ){
+            if( user.password === password ){
+                return res.status(200).json({
+                    message : 'You will be redirected to menu...',
+                    success : true
+                })
+            }else{
+                return res.status(200).json({
+                    message : 'Username already in use',
+                    success : false
+                })
+            }
+        }else{
+            user = await db.collection('User').insertOne({ 
+                username ,
+                password
+            })
+            return res.status(200).json({
+                message : 'You will be redirected to menu...',
+                success : true
+            })
+        }
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message : 'Something went wrong'
+        })
+    }
+}
